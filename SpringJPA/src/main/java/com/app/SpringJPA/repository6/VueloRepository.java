5e9e46fab88051c07e2bf77ab8191b7076fbc5dc
fercho78z <fbt2031@gmail.com>
2024-09-29 14:32:33 -0600

    MS-APIGATEWAY-EUREKA-JWT-01

--- SpringJPA/src/main/java/com/app/SpringJPA/repository6/VueloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.SpringJPA.repository6;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.app.SpringJPA.entities6.Vuelo;
-@Repository
-public interface VueloRepository extends JpaRepository<Vuelo, Long> {
-
-}
